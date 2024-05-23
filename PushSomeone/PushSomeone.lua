dac05010dbcac009c77de03273a773428745339f
Tenebrosful <hugobernard39@gmail.com>
2024-05-23 21:20:47 +0200

    Set func to local

--- PushSomeone/PushSomeone.lua
+++ PushSomeone/PushSomeone.lua
@@ -9,35 +9,35 @@ local microgameMusic = {
   LoadResource(contentRoot .. "\\Music\\PushSomeone_155.ogg", ResourceType.Audio),
 }
 
-local defaultVariation = CreateMicrogameVariation("default", OnBeginMicrogame_default, OnMicrogameTick_default,
-  OnPostMicrogame_default)
-
-function OnBeginMicrogame_default()
+local function OnBeginMicrogame_default()
   print("Microgame has begun!")
 end
 
-function OnMicrogameTick_default(fTimeLeft)
+local function OnMicrogameTick_default(fTimeLeft)
   print("Microgame has ticked! There are " .. tostring(fTimeLeft) .. " seconds")
 end
 
-function OnPostMicrogame_default()
+local function OnPostMicrogame_default()
+  print("Microgame has ended!")
+end
+
+local defaultVariation = CreateMicrogameVariation("default", OnBeginMicrogame_default,
+  OnMicrogameTick_default, OnPostMicrogame_default)
+
+local function OnBeginMicrogame_definedPlayer()
+  print("Microgame has begun!")
+end
+
+local function OnMicrogameTick_definedPlayer(fTimeLeft)
+  print("Microgame has ticked! There are " .. tostring(fTimeLeft) .. " seconds")
+end
+
+local function OnPostMicrogame_definedPlayer()
   print("Microgame has ended!")
 end
 
 local definedPlayerVariation = CreateMicrogameVariation("definedPlayer", OnBeginMicrogame_definedPlayer,
   OnMicrogameTick_definedPlayer, OnPostMicrogame_definedPlayer)
-
-function OnBeginMicrogame_definedPlayer()
-  print("Microgame has begun!")
-end
-
-function OnMicrogameTick_definedPlayer(fTimeLeft)
-  print("Microgame has ticked! There are " .. tostring(fTimeLeft) .. " seconds")
-end
-
-function OnPostMicrogame_definedPlayer()
-  print("Microgame has ended!")
-end
 
 microgame = CreateMicrogame("PushSomeone", microgameMusic, { defaultVariation, definedPlayerVariation },
   { Difficulty = Difficulty.Easy, Rarity = 8, MinPlayers = 1, Type = MicrogameType.WinBeforeEnd, PlayEffect = true })
