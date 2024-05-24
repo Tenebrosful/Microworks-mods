047b13b1034afe1413da2df9bcabf27601ba38ba
Tenebrosful <hugobernard39@gmail.com>
2024-05-24 22:33:57 +0200

    Add looking at a player logic

--- LookAtSomeone/LookAtSomeone.lua
+++ LookAtSomeone/LookAtSomeone.lua
@@ -10,6 +10,8 @@ local microgameMusic = {
   LoadResource(contentRoot .. "\\Music\\LookAtSomeone_155.ogg", ResourceType.Audio),
 }
 
+local lPlayer = worldInfo:GetLocalPlayer()
+
 local function VectorToString(vector)
   return string.format("(%f %f %f)", vector.x, vector.y, vector.z)
 end
@@ -36,7 +38,38 @@ local function OnBeginMicrogame_default()
 end
 
 local function OnMicrogameTick_default(fTimeLeft)
+  --if coordinator:IsMarkedAWinner(lPlayer:GetNetworkID()) then return end
 
+  ShowMessageLocal("[Player Camera] " ..
+    VectorToString(lPlayer:GetCameraPosition()) .. " looking at " .. VectorToString(lPlayer:GetCameraDirection()))
+  local hitPoints = CastRayAll(lPlayer:GetCameraPosition(), lPlayer:GetCameraDirection(), 60)
+  local playerFound
+  if hitPoints ~= nil then
+    for i, hitPoint in ipairs(hitPoints) do
+      local object = hitPoint:GetHitObject()
+      print("[Object looking] " ..
+        object:GetObjectName() ..
+        " is " ..
+        object:GetObjectType() ..
+        " tags: " ..
+        object:GetTag() ..
+        " NetworkID : " .. object:GetNetworkID() .. " at " .. VectorToString(hitPoint:GetHitPosition()))
+      if string.find(object:GetTag(), "Player") ~= nil then
+        playerFound = GetPlayerByID(object:GetNetworkID())
+        ShowMessageLocal("Looking at " .. playerFound:GetSteamName())
+        break;
+      end
+    end
+  else
+    ShowMessageLocal("[Player looking] Looking at nothing")
+  end
+
+  if playerFound ~= nil
+  then
+    coordinator:MarkWinner(lPlayer:GetNetworkID(), false)
+  else
+    coordinator:UnmarkWinner(lPlayer:GetNetworkID(), false)
+  end
 end
 
 local function OnPostMicrogame_default()
@@ -47,4 +80,4 @@ local defaultVariation = CreateMicrogameVariation("default", OnBeginMicrogame_de
   OnMicrogameTick_default, OnPostMicrogame_default)
 
 microgame = CreateMicrogame("LookAtSomeone", microgameMusic, { defaultVariation },
-  { Difficulty = Difficulty.Easy, Rarity = 8, MinPlayers = 2, Type = MicrogameType.WinBeforeEnd, PlayEffect = true })
+  { Difficulty = Difficulty.Easy, Rarity = 8, MinPlayers = 1, Type = MicrogameType.WinBeforeEnd, PlayEffect = false })
