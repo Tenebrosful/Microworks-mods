9be3864eee20fc930481fbca67a3c400e1ee352f
Tenebrosful <hugobernard39@gmail.com>
2024-05-24 03:05:18 +0200

    Finished microgame

--- PushSomeone/PushSomeone.lua
+++ PushSomeone/PushSomeone.lua
@@ -2,13 +2,15 @@ local microgame
 local contentRoot = "\\Microgames\\PushSomeone"
 local coordinator = worldInfo:GetCoordinator()
 local microgameMusic = {
-  LoadResource(contentRoot .. "\\Music\\PushSomeone_100.ogg", ResourceType.Audio),
+  LoadResource(contentRoot .. "\\Music\\PushSomeone_100.ogg", ResourceType.Audio), -- That's Microgame 15_01 from the game file
   LoadResource(contentRoot .. "\\Music\\PushSomeone_111.ogg", ResourceType.Audio),
   LoadResource(contentRoot .. "\\Music\\PushSomeone_122.ogg", ResourceType.Audio),
   LoadResource(contentRoot .. "\\Music\\PushSomeone_133.ogg", ResourceType.Audio),
   LoadResource(contentRoot .. "\\Music\\PushSomeone_144.ogg", ResourceType.Audio),
   LoadResource(contentRoot .. "\\Music\\PushSomeone_155.ogg", ResourceType.Audio),
 }
+
+local pushListenner
 
 -- Is local player the server?
 local function IsServer()
@@ -19,35 +21,87 @@ end
 local function GetPlayerByID(playerID)
   return worldInfo:GetPlayerByID(playerID)
 end
+
+-- Default Variation
 local function OnBeginMicrogame_default()
-  print("Microgame has begun!")
+  pushListenner = ListenFor("PlayerPushed",
+    function(pay)
+      if coordinator:IsMarkedAWinner(pay:GetPusherPlayer():GetNetworkID()) then return end -- return early if already winner
+
+      coordinator:MarkWinner(pay:GetPusherPlayer():GetNetworkID(), false)
+    end)
 end
 
-local function OnMicrogameTick_default(fTimeLeft)
-  print("Microgame has ticked! There are " .. tostring(fTimeLeft) .. " seconds")
-end
+local function OnMicrogameTick_default(fTimeLeft) end
 
 local function OnPostMicrogame_default()
-  print("Microgame has ended!")
+  RemoveEventListener(pushListenner)
 end
 
 local defaultVariation = CreateMicrogameVariation("default", OnBeginMicrogame_default,
   OnMicrogameTick_default, OnPostMicrogame_default)
 
-local function OnBeginMicrogame_definedPlayer()
-  print("Microgame has begun!")
+-- Defined player Variation
+local playerAssignement = {} -- Table used by server to know who must push who
+local playerToPush           -- PlayerName used by Client to know who he has to push
+
+local function assignPlayerTarget(allActivePlayers)
+  for i, player in ipairs(allActivePlayers) do
+    playerAssignement[player:GetNetworkID()] = allActivePlayers[math.random(#allActivePlayers)]:GetNetworkID()
+
+    if (player:GetNetworkID() == playerAssignement[player:GetNetworkID()]) then
+      local preventInfinite = 0
+      while player:GetNetworkID() == playerAssignement[player:GetNetworkID()] and preventInfinite < 5 do
+        preventInfinite = preventInfinite + 1
+        print("[PushSomeone] Player have to push himselft ! Re-assigning (" .. preventInfinite .. ")")
+        playerAssignement[player:GetNetworkID()] = allActivePlayers[math.random(#allActivePlayers)]:GetNetworkID()
+
+        if preventInfinite == 5 and player:GetNetworkID() == playerAssignement[player:GetNetworkID()] then -- RNG failed 5 times
+          if player:GetNetworkID() ~= allActivePlayers[1]:GetNetworkID() then
+            playerAssignement[player:GetNetworkID()] = allActivePlayers[1]:GetNetworkID()                  -- Assign first player in player list
+          else
+            playerAssignement[player:GetNetworkID()] = allActivePlayers[2]:GetNetworkID()                  -- Assign seconds player in player list because the first one is himselft (probably never happen)
+          end
+        end
+      end
+    end
+  end
 end
 
-local function OnMicrogameTick_definedPlayer(fTimeLeft)
-  print("Microgame has ticked! There are " .. tostring(fTimeLeft) .. " seconds")
+RPC("PushSomeone_PlayerToPush", SendTo.TargetClient, Delivery.Reliable, function(playerID)
+  playerToPush = GetPlayerByID(playerID):GetSteamName()
+  microgame:SetTranslationData("definedPlayer", playerToPush)
+end)
+
+local function OnBeginMicrogame_definedPlayer()
+  if IsServer() then
+    assignPlayerTarget(worldInfo:GetAllActivePlayers())
+    for player, target in pairs(playerAssignement) do
+      worldGlobals.PushSomeone_PlayerToPush(GetPlayerByID(player), target)
+    end
+  end
+
+  pushListenner = ListenFor("PlayerPushed",
+    function(pay)
+      if coordinator:IsMarkedAWinner(pay:GetPusherPlayer():GetNetworkID()) then return end -- return early if already winner
+
+      if playerAssignement[pay:GetPusherPlayer():GetNetworkID()] == pay:GetPushedPlayer():GetNetworkID() then
+        coordinator:MarkWinner(pay:GetPusherPlayer():GetNetworkID())
+      end
+    end)
 end
+
+local function OnMicrogameTick_definedPlayer(fTimeLeft) end
 
 local function OnPostMicrogame_definedPlayer()
-  print("Microgame has ended!")
+  RemoveEventListener(pushListenner)
+  microgame:ResetTranslationData()
+  playerAssignement = {}
+  playerToPush = nil
 end
 
 local definedPlayerVariation = CreateMicrogameVariation("definedPlayer", OnBeginMicrogame_definedPlayer,
-  OnMicrogameTick_definedPlayer, OnPostMicrogame_definedPlayer)
+  OnMicrogameTick_definedPlayer, OnPostMicrogame_definedPlayer, 6)
 
 microgame = CreateMicrogame("PushSomeone", microgameMusic, { defaultVariation, definedPlayerVariation },
-  { Difficulty = Difficulty.Easy, Rarity = 8, MinPlayers = 1, Type = MicrogameType.WinBeforeEnd, PlayEffect = true })
+  { Difficulty = Difficulty.Easy, Rarity = 8, MinPlayers = 2, Type = MicrogameType.WinBeforeEnd, PlayEffect = true })
